81a3ef7117bd4974b5ea6da7cc84c8317b699a72
Thingus <john.roberts@flowminder.org>
2025-08-03 15:09:26 +0100

    Minor fixes

--- src/app/frontpage/explanation/page.tsx
+++ src/app/frontpage/explanation/page.tsx
@@ -12,19 +12,18 @@ export default function Page() {
       <ul>
         <li>Every cell has a constant land level and a variable standing water level</li>
         <li>
-          Every cell can also have water flowing over it if it does not have standing
-          water
+          Every cell can also have water flowing over it or not.
         </li>
         <li>If a cell is lower than a neighbour with water flowing, that cell now has water flowing.</li>
         <li>If a cells land + water is the lowest of its neighbours, and it has water flowing, it no longer has water flowing - it&apos;s water level increases randomly by 1 or 0.</li>
       </ul>
-      <p>The simulation is initialised by setting an arbitrary cell to have water flow - you can add more random springs with the `fountain button`</p>
+      <p>The simulation is initialised by setting an arbitrary cell to have water flow - you can add more random springs with the &lsquot;fountain&rsquot; button</p>
       <h2>Implementation</h2>
-      This is implemented in Rust, using the <Link href="https://github.com/wasm-bindgen/wasm-bindgen/tree/main">wasm-bindgen</Link> crate - see <Link href="https://github.com/thingus/canvas-messing">here</Link> for the full implementation.
+      This is implemented in Rust, using the <Link href="https://github.com/wasm-bindgen/wasm-bindgen/tree/main">wasm-bindgen</Link> crate - see <Link href="https://github.com/thingus/canvas_messing">here</Link> for the full implementation.
       <h2>Optimisations</h2>
-      The actual algorithm is mainly dumb loops, nothing too fancy. The tick() functionis <b>far</b> lighter than the draw(), so that&apos;s where I&apos;ve focussed the first pass. <br />
-      This library uses Rust&apos;s web-sys crate to call the needed Web API functions; all the ones we care about use the Context2D object that we get from Javascript. The first iteration of draw() looped through every cell of the simulation and called `set_fill_style(color)` followed by `fill_rect(cell_size)`. This revealed unto me a truth; the wasm-JS-webAPI chain is <b>extremely slow</b>, and calls to it should be kept to a minimum.<br />
-      Instead, I took the masochistic route. I replaced the calls to set_fill_style() and fill_rect() with an in-memory bitmap that lives in wasm; this gets written to the canvas via `put_image_data()`, replacing [width*length] calls to the webAPI per draw() to just one single call. This seems to work pretty well - there&apos;s still jank appearing in Firefox&apos;s profiler, but it doesn&apos;t kill the UI loop completely. All it took was a day of swearing at Rust indexing errors, then stomping off to bed in a huff and finding that I&apos;d got an x and y arg the wrong way around the next morning, teaching me for the seventh time a valuable lesson about passing those as structs.<br />
+      The actual algorithm is mainly dumb loops, nothing too fancy. The <code>tick()</code> functionis <b>far</b> lighter than the <code>draw(),</code> so that&apos;s where I&apos;ve focussed the first pass. <br />
+      This library uses Rust&apos;s web-sys crate to call the needed Web API functions; all the ones we care about use the Context2D object that we get from Javascript. The first iteration of <code>draw()</code> looped through every cell of the simulation and called <code>set_fill_style(color)</code> followed by <code>fill_rect(cell_size)</code>. This revealed unto me a truth; the wasm-JS-webAPI chain is <b>extremely slow</b>, and calls to it should be kept to a minimum.<br />page
+      Instead, I took the masochistic route. I replaced the calls to <code>set_fill_style()</code> and <code>fill_rect()</code> with an in-memory bitmap that lives in wasm; this gets written to the canvas via <code>put_image_data()</code>, replacing <code>[width*length]</code> calls to the webAPI per <code>draw()</code> to just one single call. This seems to work pretty well - there&apos;s still jank appearing in Firefox&apos;s profiler, but it doesn&apos;t kill the UI loop completely. All it took was a day of swearing at Rust indexing errors, then stomping off to bed in a huff and finding that I&apos;d got an x and y arg the wrong way around the next morning, teaching me for the seventh time a valuable lesson about passing coordinates as structs.<br />
       <h2>Further work</h2>
       <ul>
         <li>This shouldn&apos;t be living in the UI thread - ideally this should be farmed off to a webworker and an off-screen canvas that can be blitted in on the clock interrupt in JS.</li>
